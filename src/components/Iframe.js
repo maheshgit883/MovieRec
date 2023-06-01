f0a41df87bb131e80f63712ebdca69b0eab03bb2
Mahesh Jagtap <78598516+maheshgit883@users.noreply.github.com>
2023-06-02 00:40:22 +0530

    Add files via upload

---
+++ src/components/Iframe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Iframe = () => {
+  return (
+    <div>
+      <iframe
+        src="https://movix-eta.vercel.app/" // Replace with the URL of the website you want to render
+        title="Website"
+        width="100%"
+        height="600px"
+      />
+    </div>
+  );
+};
+
+export default Iframe;
